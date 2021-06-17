a97d5ee8dadd152e89355e56e71aebee85323e34
conniewambui <consolatawambuimacharia@gmail.com>
2021-06-17 11:03:32 +0300

    first commit

---
+++ test.py
@@ -0,0 +1,46 @@
+q=([100, 110, 120, 130, 140])
+q.__add__(5)
+print(q)
+
+n = [1, 2, 3, 4, 5, 6, 7, 8, 9,10]
+n.__module__(3)
+
+
+k=([[1,2],[3,4],[5,6]])
+k.extend(k) 
+
+p=([3,6,8,2,4,1,5,1])
+p.count(p)
+
+
+x=['a','b','a','e','d','b','c','e','f','g','h']
+x.reverse(x)
+
+x=300;
+(x=200,x<300,x++)
+x.conjugate(x)
+else
+x.__module__(7)
+
+class Account:
+       def __init__(self,name,age):
+    Self.name=name
+    self.age=age
+    if Student <0:
+           return f"Dear {self.name}  you were boarn on 2002"
+    elif self.name<Student:
+        
+        if Student:
+               return f"Dear {self.name}  you were boarn on 2003"
+    elif self.name<Student:
+        
+        
+        if Student:
+               return f"Dear {self.name}  you were boarn on 2000"
+    elif self.name<Student:
+        
+       if Student:
+               return f"Dear {self.name} you were boarn on 2000"
+    elif self.name<Student:
+        
+}}
